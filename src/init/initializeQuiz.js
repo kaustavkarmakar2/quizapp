882f530d6cb72ce3f57e8208a287804a442f6bad
yahya ganjo <yahya.ganjo@gmail.com>
2021-05-30 14:27:39 +0200

    making start buttom

--- src/init/initializeQuiz.js
+++ src/init/initializeQuiz.js
@@ -6,23 +6,32 @@ import createDOMElement from "../utils/createDOMElement.js";
 import getDOMElement from "../utils/getDOMElement.js";
 import createNextQuestionButtonElement from "../views/createNextQuestionButtonElement.js";
 import { quizData } from '../data.js';
+import clearDOMElement from '../utils/clearDOMElement.js';
 
 const initializeQuiz = () => {
     quizData.currentQuestionIndex = 0;
 
     console.log('bla');
-    setupQuizHTML();
-
-    showCurrentQuestion();
+    startButton();
 }
 
+const startButton = () => {
+    const userInterfaceContainer = document.querySelector('#user-interface');
+    const startBtn = document.createElement('button')
+    startBtn.textContent = 'Start Quiz';
+    startBtn.addEventListener('click', () => {
+        clearDOMElement(userInterfaceContainer);
+        setupQuizHTML();
+        showCurrentQuestion();
+    })
+    userInterfaceContainer.appendChild(startBtn)
+}
 const setupQuizHTML = () => {
     const userInterfaceContainer = getDOMElement('user-interface');
     const quizContainer = createDOMElement('div', { id: QUIZ_CONTAINER_ID });
     const questionContainer = createDOMElement('div', { id: QUESTION_CONTAINER_ID });
-
     quizContainer.appendChild(questionContainer);
-
+// add a previous button
     const nextQuestionButton = createNextQuestionButtonElement();
     quizContainer.appendChild(nextQuestionButton);
 
